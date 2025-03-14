309401c53d8a4f5f7689ce63b73e779161803cec
FizzCashman <git@lewismacleod.net>
2025-03-14 12:08:41 +0000

    1.2: Modified circleshap, constants, main & player. Created asteroid & asteroid field. Game draws player ship and circle asteroids with player collision.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+
+Player.containers = (updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
+AsteroidField.containers = (updatable,)
 
 def main():
     pygame.init()
@@ -11,12 +22,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision_check(player):
+                sys.exit("GAME OVER!")
         screen.fill((0,0,0))
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 

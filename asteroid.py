60b1bf661b02909f9573f6a353e73e96b69a90c7
FizzCashman <git@lewismacleod.net>
2025-05-07 15:39:28 +0100

    1.5: Modified asteroid.py so asteroids now split correctly when shot

--- asteroid.py
+++ asteroid.py
@@ -21,3 +21,7 @@ class Asteroid(CircleShape):
             new_velocity1 = self.velocity.rotate(random.uniform(20, 50))
             new_velocity2 = self.velocity.rotate(-random.uniform(20, 50))
             new_radius = self.radius - ASTEROID_MIN_RADIUS
+            new_asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+            new_asteroid1.velocity = new_velocity1 * 1.2
+            new_asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+            new_asteroid2.velocity = new_velocity2 * 1.2
